d2b518cdf8cc4601dd2ca49ca3f6f6418753fcad
juanigarciadev <juanigarciadev@gmail.com>
2023-09-01 13:22:29 -0300

    deleted tools logos in projects and added tags

--- projects.js
+++ projects.js
@@ -1,100 +1,55 @@
-const html =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg";
-const css =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg";
-const react =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg";
-const bootstrap =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg";
-const sass =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg";
-const git =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg";
-const firebase =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg";
-const nodejs =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg";
-const photoshop =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-line.svg";
-const illustrator =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-line.svg";
-const visualstudio =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain.svg";
-const figma =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg";
-const netlify =
-  "https://res.cloudinary.com/diruiumfk/image/upload/v1693541812/netlify_idabbe.png";
-const vercel =
-  "https://res.cloudinary.com/diruiumfk/image/upload/v1693541717/vercel_f7ffde.png";
-
 const projects = [
   {
     name: "TODOTECH",
     information:
       "My first project in Web Development, the idea started from the creation of an e-commerce. At first it was just the layout (HTML and CSS), but later I added vanilla JavaScript to add e-commerce, dark mode and login functionalities. It still has the original design that I did in its day but probably at some point of boredom I will make a second version updating everything with my knowledge of the moment.",
-    tools: [
-      [html, "HTML5"],
-      [css, "CSS3"],
-      [bootstrap, "Bootstrap"],
-      [sass, "SASS"],
-      [git, "git"],
-      [nodejs, "NodeJS"],
-      [photoshop, "Photoshop"],
-      [illustrator, "Illustrator"],
-      [visualstudio, "Visual Studio Code"],
-      [netlify, "Netlify"],
-    ],
+    tools:
+      "HTML5, CSS3, Bootstrap, SASS, Photoshop, Illustrator, git, Visual Studio Code and Netlify.",
     img: "https://res.cloudinary.com/diruiumfk/image/upload/v1693254750/todotech_yk7ask.png",
-    logo: "https://res.cloudinary.com/diruiumfk/image/upload/v1692741440/todotech-logo_dwbnel.png",
+    logo: "https://res.cloudinary.com/diruiumfk/image/upload/v1693585257/todotech-logo_whcfxr.png",
     url: "https://todotech.netlify.app/",
     repository: "https://github.com/juanigarciadev/proyecto-TODOTECH",
     badge: false,
+    tag: "E-COMMERCE",
   },
   {
     name: "zolution",
     information:
       "Own business in creation. It is a project that seeks to unite a team of designers and developers from around the world through which competent web pages and applications are created that can enter the market quickly, professionally and with a competitive price accompanying the customer from start to finish.",
-    tools: [
-      [html, "HTML5"],
-      [css, "CSS3"],
-      [react, "ReactJS"],
-      [sass, "SASS"],
-      [git, "git"],
-      [nodejs, "NodeJS"],
-      [photoshop, "Photoshop"],
-      [illustrator, "Illustrator"],
-      [visualstudio, "Visual Studio Code"],
-      [figma, "Figma"],
-      [vercel, "Vercel"],
-    ],
+    tools:
+      "HTML5, CSS3, ReactJS, SASS, git, Photoshop, Illustrator, Figma, Visual Studio Code and Vercel.",
     img: "https://res.cloudinary.com/diruiumfk/image/upload/v1693254749/zolution_dhcyxw.png",
     logo: "https://res.cloudinary.com/diruiumfk/image/upload/v1684452439/zolution-logo_uekblv.svg",
     url: "https://zolution.vercel.app/",
     repository: "https://github.com/juanigarciadev/zolution",
     badge: "Working in...",
+    tag: "DESIGN AND DEVELOPMENT AGENCY",
   },
   {
     name: "BEYOND IMAGINATION",
     information:
       "My first application applying ReactJS as a framework. It is an online learning academy based on an e-commerce hosted on Firebase, with an interactive catalog and purchase number. Soon the platform of the academy will be added in which you can register a new account or log in to an already created one, view the courses purchased and their contents.",
-    tools: [
-      [html, "HTML5"],
-      [css, "CSS3"],
-      [react, "ReactJS"],
-      [sass, "SASS"],
-      [git, "git"],
-      [nodejs, "NodeJS"],
-      [firebase, "Firebase"],
-      [photoshop, "Photoshop"],
-      [illustrator, "Illustrator"],
-      [visualstudio, "Visual Studio Code"],
-      [vercel, "Vercel"],
-    ],
+    tools:
+      "HTML5, CSS3, ReactJS, Firebase, SASS, git, Photoshop, Illustrator, Figma, Visual Studio Code and Vercel.",
     img: "https://res.cloudinary.com/diruiumfk/image/upload/v1693254750/beyond-imagination_p25riu.png",
     logo: "https://res.cloudinary.com/diruiumfk/image/upload/v1680216082/beyond-imagination-full_jxvtga.png",
     url: "https://beyond-imagination.vercel.app/",
     repository: "https://github.com/juanigarciadev/first-react-app",
     badge: false,
+    tag: "E-COMMERCE",
+  },
+  {
+    name: "My portfolio",
+    information:
+      "My first application applying ReactJS as a framework. It is an online learning academy based on an e-commerce hosted on Firebase, with an interactive catalog and purchase number. Soon the platform of the academy will be added in which you can register a new account or log in to an already created one, view the courses purchased and their contents.",
+    tools:
+      "HTML5, CSS3, ReactJS (Vite), Tailwind CSS, git, Photoshop, Illustrator, Visual Studio Code and Vercel.",
+    img: "https://res.cloudinary.com/diruiumfk/image/upload/v1693582911/portfolio_i9ioh1.png",
+    logo: null,
+    url: "https://juanigarciadev.vercel.app/",
+    repository: "https://github.com/juanigarciadev/my-portfolio",
+    badge: false,
+    tag: "PERSONAL PORTFOLIO",
   },
 ];
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import projects from "../../../projects";
+import { RxExternalLink } from "react-icons/rx";
 
 const Projects = () => {
   return (
-    <section className="flex flex-col pb-32 pt-16 gap-32 lg:gap-16 h-auto">
+    <section className="grid grid-cols-2 pb-32 pt-16 px-16 gap-16 lg:flex lg:flex-col lg:gap-18 h-auto">
       {projects.map((project) => {
         return (
           <div
-            className="grid grid-cols-2 justify-center items-top px-24 gap-6 hover:scale-105 duration-300 lg:flex lg:flex-col lg:px-20 md:hover:scale-100"
+            className="flex flex-col px-8 gap-6 hover:scale-105 duration-300 lg:flex lg:flex-col lg:px-20 md:hover:scale-100"
             key={project.name}
           >
             <div>
               <img src={project.img} className="rounded-xl w-full" alt="" />
             </div>
             <div className="flex flex-col w-full gap-6 justify-top">
+              <h3 className="text-sm text-neutral-400">{project.tag}</h3>
               <div>
-                <img src={project.logo} className="w-64" alt="" />
+                {project.logo != null ? (
+                  <img src={project.logo} className="w-64" alt="" />
+                ) : (
+                  <h3 className="text-title font-bold pb-1.5">
+                    {project.name}
+                  </h3>
+                )}
               </div>
               {project.badge ? (
                 <h3 className="flex justify-center items-center text-md text-black bg-yellow-400 w-32 h-8 rounded-md">
                   {project.badge}
                 </h3>
               ) : null}
-              <h3>{project.information}</h3>
-              <ul className="flex gap-4 flex-wrap">
+              <h3 className="text-md">{project.information}</h3>
+              <h3 className="text-md">
+                <span className="font-bold">Built with:</span> {project.tools}
+              </h3>
+              {/* <ul className="flex gap-4 flex-wrap">
                 {project.tools.map((tools) => {
                   return (
                     <li key={tools}>
@@ -36,19 +47,21 @@ const Projects = () => {
                     </li>
                   );
                 })}
-              </ul>
+              </ul> */}
               <div className="flex gap-6 justify-end lg:justify-center">
                 <button
-                  className="secondaryButton"
+                  className="secondaryButton gap-1"
                   onClick={() => window.open(project.repository)}
                 >
-                  View repository
+                  View the code
+                  <RxExternalLink />
                 </button>
                 <button
-                  className="button"
+                  className="button gap-1"
                   onClick={() => window.open(project.url)}
                 >
-                  Visit website
+                  Visit the app
+                  <RxExternalLink />
                 </button>
               </div>
             </div>
