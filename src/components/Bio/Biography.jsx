1625c868cf98fa432e5a659e39fcbb7e43c5d416
juanigarciadev <juanigarciadev@gmail.com>
2023-09-06 23:04:16 -0300

    minor fix

--- src/components/Bio/Biography.jsx
+++ src/components/Bio/Biography.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Biography = () => {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      Bio
+      Biography
     </div>
   );
 };
