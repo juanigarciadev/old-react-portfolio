e204592d332872a4d35f894c25afe2fa66a8ad75
juanigarciadev <juanigarciadev@gmail.com>
2024-04-23 18:57:03 -0300

    Add: Visit site button in projects section cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -28,10 +28,16 @@ const Projects = () => {
                 className="rounded-2xl bg-cover w-full h-[300px] duration-[1000ms] hover:bg-bottom"
                 style={style}
               ></section>
-              <Link to={project.moreInfo} className="flex items-center justify-center w-8 h-8 bg-white shadow-md border rounded-full absolute bottom-4 right-4 group-hover:w-36 group-hover:justify-center duration-300 cursor-pointer">
-                <span className="w-fit h-4 pl-1 absolute opacity-0 text-sm text-black overflow-hidden group-hover:relative group-hover:opacity-100 group-hover:duration-400">More information</span>
-                <svg width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" d="m11.293 17.293l1.414 1.414L19.414 12l-6.707-6.707l-1.414 1.414L15.586 11H6v2h9.586z"/></svg>
-              </Link>
+              <section className="flex flex-col items-end gap-2 absolute bottom-4 right-4">
+                <Link to={project.url} target="_blank" className="flex items-center justify-center w-8 h-8 gap-[3px] bg-white shadow-md border rounded-full group-hover:w-24 group-hover:justify-center duration-300 cursor-pointer">
+                  <span className="w-fit h-4 pl-1 absolute opacity-0 text-sm text-black overflow-hidden group-hover:relative group-hover:opacity-100 group-hover:duration-400">Visit site</span>
+                  <svg width="0.8em" height="0.8em" viewBox="0 0 24 24"><path fill="black" d="m13 3l3.293 3.293l-7 7l1.414 1.414l7-7L21 11V3z"/><path fill="black" d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2z"/></svg>
+                </Link>
+                <Link to={project.moreInfo} className="flex items-center justify-center w-8 h-8 bg-[#00ff00] shadow-md border border-[#56db3e] rounded-full group-hover:w-36 group-hover:justify-center duration-300 cursor-pointer">
+                  <span className="w-fit h-4 pl-1 absolute opacity-0 text-sm text-black overflow-hidden group-hover:relative group-hover:opacity-100 group-hover:duration-400">More information</span>
+                  <svg width="1em" height="1em" viewBox="0 0 24 24"><path fill="black" d="m11.293 17.293l1.414 1.414L19.414 12l-6.707-6.707l-1.414 1.414L15.586 11H6v2h9.586z"/></svg>
+                </Link>
+              </section>
             </article>
           );
         })}
